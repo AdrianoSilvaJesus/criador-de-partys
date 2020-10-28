394337451f71f2b3bdc4e248d5be7e46a6037550
AdrianoSilvaJesus <adrianosilvaoliveiradejesus@outlook.com>
2020-10-27 22:00:15 -0300

    Refatoração

--- public/js/Construtor.js
+++ public/js/Construtor.js
@@ -1,77 +1,38 @@
-function CreatPainel() {
-    // Setando Vars
-    var Painel = document.createElement("DIV");
-    var Close = document.createElement("DIV");
-    var Protecao = document.createElement("DIV");
+class Painel{
+    constructor(){
+        this.verificaPainel = false;
+    }
 
-    // Setando Ids
-    Painel.id = "Painel";
-    Close.id = "Close";
-    Protecao.id = "Protecao";
+    renderPainel(color = null, imageUrl = null){
+        // Setando Vars
+        var Painel= document.createElement("DIV");
+        var Close = document.createElement("DIV");
+        var Protecao = document.createElement("DIV");
 
-    // Setando Conteudos
-    Close.innerText = "X";
+        // Setando Ids
+        Painel.id = "Painel";
+        Close.id = "Close";
+        Protecao.id = "Protecao";
 
-    // Setando Pais
-    document.getElementById("PainelControle").appendChild(Protecao);
-    document.getElementById("Protecao").appendChild(Painel);
-    document.getElementById("Painel").appendChild(Close);
+        // Setando estilos
+        Painel.style.background = color;
+        if (imageUrl) Painel.style.backgroundImage = "url('../public/img/back1.jpg')";
 
-    // Colocando Eventos
-    Close.addEventListener("click", ClosePainel);
-    
-    // Funções Externas
-    TaAbertoDiacho = true;
-    Resize();
+        // Setando Conteudos
+        Close.innerText = "X";
+
+        // Setando Pais
+        document.getElementById("PainelControle").appendChild(Protecao);
+        document.getElementById("Protecao").appendChild(Painel);
+        document.getElementById("Painel").appendChild(Close);
+
+        Close.addEventListener("click", this.closePainel);
+        !this.verificaPainel;
+    }
+
+    closePainel(){
+        document.getElementById("Protecao").remove();
+        !this.verificaPainel;
+    }
 };
 
-function TicketAberto () {
-    
-}
-
-function CriandoTicket () {
-    var Titulo = document.createElement("H2");
-    Titulo.id = "Titulo";
-    Titulo.innerText = "Criando um Ticket";
-    document.getElementById("Painel").appendChild(Titulo);
-    var Titulo = document.createElement("div");
-    Titulo.id = "Container2";
-    document.getElementById("Painel").appendChild(Titulo);
-
-    const Nomes = ['Titulo','Descricao','Instancia','Nivel','Horario','Valor','Owner','Vaga1','Vaga2','Vaga3','Vaga4','Vaga5','Owner2','Vaga6','Vaga7','Vaga8','Vaga9','Vaga10']
-
-    var Style = false;
-
-    for (var i = 0  ; i < 18; i++) {
-        var Criando = document.createElement("Label");
-        var Criando2 = document.createElement("input");
-        
-        if (i >1) {if (i <5){
-            var Criando2 = document.createElement("select");  
-        }}
-        
-        if (Style === false) {
-            Style = true;
-            var Labels = "label Style";
-            var Interativos = "input Style";
-        }else{
-            Style = false;
-            var Labels = "label1 Style1";
-            var Interativos = "input1 Style1";
-        }
-
-        // Labels
-        Criando.id = "p"+i;
-        Criando.classList = Labels;
-        Criando.innerText = Nomes[i];
-        Criando.style.gridArea = 'l'.concat(i);
-        document.getElementById("Container2").appendChild(Criando);
-
-        // Interativos
-        Criando2.id = "I"+i;
-        Criando2.classList = Interativos;
-        Criando2.style.gridArea = 'i'.concat(i);
-        Criando2.value = Nomes[i];
-        document.getElementById("Container2").appendChild(Criando2);
-     }
-}
--- public/js/Painel.js
+++ public/js/Painel.js
@@ -1,39 +1,14 @@
-TaAbertoDiacho = false;
-
-function ValidarSeTaAberto() {
-    console.log(TaAbertoDiacho)
-    if (TaAbertoDiacho === true) {
-        console.log('Hello')
-        ClosePainel();  
-    }
-    CreatPainel();
-}
+const painel = new Painel();
 
 function GratisClick() {
-    ValidarSeTaAberto();
-    document.getElementById("Painel").style.background = "#6495ed99";
+    painel.renderPainel("#6495ed99");
 }
 
-function PagoClick() {
-    ValidarSeTaAberto();
-    document.getElementById("Painel").style.background = "#a52a2a99";
+function PagoClick(){
+    painel.renderPainel("#a52a2a99");
 }   
 
 function CriarTicket() {
-    ValidarSeTaAberto();
-    document.getElementById("Painel").style.background = "#ffffff";
-    document.getElementById("Painel").style.backgroundImage = "url('../public/img/back1.jpg')";
-    console.log(document.getElementById("Painel").offsetHeight);
-    console.log(document.getElementById("Painel").offsetWidth);
-    if (document.getElementById("Painel").offsetHeight <= (document.getElementById("Painel").offsetWidth)*0.7){
-        document.getElementById("Painel").style.backgroundSize = "100% 100%";
-    }else{
-        document.getElementById("Painel").style.backgroundSize = "auto";
-    };
-    CriandoTicket();
-}
-
-function ClosePainel() {  
-    document.getElementById("Protecao").remove();
-    TaAbertoDiacho = false;
+    painel.renderPainel("#ffffff", "url('../public/img/back1.jpg')");
+    console.log("Criando Ticket...")
 }
