de7bcdda6b7bdb27b283845301386d6584ea19d0
Juniorboy37 <pabloabias37@gmail.com>
2020-10-30 11:18:43 -0300

    Tickets e Painel responsivamente Responsivos.

--- public/js/Construtor.js
+++ public/js/Construtor.js
@@ -3,7 +3,7 @@ class Painel{
         this.verificaPainel = false;
     };
     
-renderPainel(color = null, imageUrl = null){
+    renderPainel(color = null, imageUrl = null){
         if (document.getElementById("Protecao") !== null) {
             document.getElementById("Protecao").remove();
         }
@@ -11,11 +11,14 @@ renderPainel(color = null, imageUrl = null){
         // Setando Vars
         var Painel= document.createElement("DIV");
         var Close = document.createElement("DIV");
+        var CloseX  = document.createElement("DIV");
         var Protecao = document.createElement("DIV");
 
-        // Setando Ids
+        // Setando Ids e Class
         Painel.id = "Painel";
         Close.id = "Close";
+        CloseX.id = "CloseX";
+        Close.className = "btn";
         Protecao.id = "Protecao";
 
         // Setando estilos
@@ -23,20 +26,23 @@ renderPainel(color = null, imageUrl = null){
         if (imageUrl) Painel.style.backgroundImage = "url('../public/img/back1.jpg')";
 
         // Setando Conteudos
-        Close.innerText = "X";
+        CloseX.innerText = "X";
 
         // Setando Pais
         document.getElementById("PainelControle").appendChild(Protecao);
         document.getElementById("Protecao").appendChild(Painel);
         document.getElementById("Painel").appendChild(Close);
+        document.getElementById("Close").appendChild(CloseX);
 
         Close.addEventListener("click", this.closePainel);
+        CloseX.addEventListener("click", this.closePainel);
         !this.verificaPainel;
     }
 
     closePainel(){
         document.getElementById("Protecao").remove();
         !this.verificaPainel;
-    }
+        }
 };
+
 
