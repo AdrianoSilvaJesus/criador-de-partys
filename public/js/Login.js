8209ed78229d95faacf2af25678ef1210a835020
Juniorboy37 <pabloabias37@gmail.com>
2020-11-14 17:06:57 -0300

    Interface Login

---
+++ public/js/Login.js
@@ -0,0 +1,8 @@
+function ClickLogin() {
+    if ( document.getElementById("LoginContainer").style.display === "") {
+        document.getElementById("LoginContainer").style.display = "none";
+    }else{
+        document.getElementById("LoginContainer").style.display = ""
+    }
+}
+
